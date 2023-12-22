dc87fcbd101c45651a059cb3343b62016990029b
yb <ylianbsila22@gmail.com>
2023-12-22 19:53:01 +0100

    ticket4

--- event-list.php
+++ event-list.php
@@ -48,7 +48,7 @@ unset($pdo);
     <?php include('includes/header.php'); ?> <!-- En-tête du site -->
 
     <div class="container">
-        <h2>"Liste des fetes</h2>
+        <h2>"Liste des fêtes</h2>
         <div class="event-container">
             <?php if (count($events) > 0) : ?>
                 <?php foreach ($events as $event) : ?>
